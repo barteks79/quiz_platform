ea6b522c29c1d6775ffd900c04ce3185902bb56a
barteks79 <sade79dev@gmail.com>
2024-10-14 16:39:07 +0200

    create cors middleware and use it in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,13 @@
-import express, {
-   type Application,
-   type Request,
-   type NextFunction,
-   type Response
-} from 'express';
+import express, { type Application } from 'express';
 
 import { json } from 'body-parser';
 import { runServer } from './util/server';
 
 import authRoutes from './routes/auth';
 import quizRouter from './routes/quiz';
-import errorMiddleware from './middlewares/error';
+
+import cors from './middlewares/cors';
+import error from './middlewares/error';
 
 const app: Application = express();
 
@@ -18,19 +15,14 @@ const app: Application = express();
 app.use(json());
 
 // HEADERS
-app.use((_req: Request, res: Response, next: NextFunction) => {
-   res.header('Access-Control-Allow-Origin', '*');
-   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-   next();
-});
+app.use(cors);
 
 // ROUTES
 app.use('/auth', authRoutes);
 app.use('/quiz', quizRouter);
 
 // ERROR
-app.use(errorMiddleware);
+app.use(error);
 
 // SERVER
 (async () => await runServer(app, 4000))();
---
+++ backend/src/middlewares/cors.ts
@@ -0,0 +1,8 @@
+import type { Request, Response, NextFunction } from 'express'
+
+export default (_req: Request, res: Response, next: NextFunction) => {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+   next();
+}
