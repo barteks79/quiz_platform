b00a14558078fce5bfdbf2c279ba40809d741261
barteks79 <sade79dev@gmail.com>
2024-11-17 19:26:12 +0100

    [backend] - feat: add slug field to quiz model, change createQuiz function so it creates slug based on quiz title

--- backend/src/controllers/quiz.ts
+++ backend/src/controllers/quiz.ts
@@ -1,237 +1,246 @@
 import { MyError, catchHandler, validateInputs, validateUserId, validateQuizId } from '../util/error';
+import slugify from 'slugify';
 
 import Question, { type IQuestion } from '../models/question';
 import Quiz, { type IQuiz } from '../models/quiz';
 import User, { type IUser } from '../models/user';
-import Rating from '../models/rating'
+import Rating from '../models/rating';
 
 import type { Response, NextFunction } from 'express';
 import type {
-   GetAllQuizzesReq,
-   GetSingleQuizReq,
-   CreateQuizReq,
-   EditQuizReq,
-   DeleteQuizReq,
+    GetAllQuizzesReq,
+    GetSingleQuizReq,
+    CreateQuizReq,
+    EditQuizReq,
+    DeleteQuizReq
 } from '../types/quiz';
 
 export const getQuizzes = async (req: GetAllQuizzesReq, res: Response, next: NextFunction) => {
-   // VALIDATION
-   const isSuccess = validateInputs(req, 'data', next);
-   if (!isSuccess) return;
+    // VALIDATION
+    const isSuccess = validateInputs(req, 'data', next);
+    if (!isSuccess) return;
 
-   const { page, perPage, ageCategory } = req.body;
+    const { page, perPage, ageCategory } = req.body;
 
-   try {
-      // QUIZZES COUNT
-      const quizzesAmount = await Quiz.countDocuments({
-         ageCategory: { $gte: ageCategory }
-      });
+    try {
+	   // QUIZZES COUNT
+	   const quizzesAmount = await Quiz.countDocuments({
+		  ageCategory: { $gte: ageCategory }
+	   });
 
-      // FOUND QUIZZES
-      const quizzes: IQuiz[] = await Quiz.find({
-         ageCategory: { $gte: ageCategory }
-      }).skip((page - 1) * perPage).limit(perPage).select('-_id title ageCategory createdAt').populate('creatorId', 'name');
+	   // FOUND QUIZZES
+	   const quizzes: IQuiz[] = await Quiz.find({
+		  ageCategory: { $gte: ageCategory }
+	   }).skip((page - 1) * perPage).limit(perPage).select('-_id title ageCategory createdAt').populate('creatorId', 'name');
 
-      res.status(200).json({ message: 'Fetched quizzes successfully.', quizzesAmount, quizzes });
-   } catch (err) {
-      catchHandler(err, next);
-   }
+	   res.status(200).json({ message: 'Fetched quizzes successfully.', quizzesAmount, quizzes });
+    } catch (err) {
+	   catchHandler(err, next);
+    }
 };
 
 export const getQuiz = async (req: GetSingleQuizReq, res: Response, next: NextFunction) => {
-   // CHECKING FOR QUIZ EXISTENCE
-   const { quizId } = req.params;
-   const existingQuiz: IQuiz | void = await validateQuizId(quizId, next);
-   if (!existingQuiz) return;
+    // CHECKING FOR QUIZ EXISTENCE
+    const { quizId } = req.params;
+    const existingQuiz: IQuiz | void = await validateQuizId(quizId, next);
+    if (!existingQuiz) return;
 
-   const quiz: IQuiz | null = await Quiz.findById(quizId).select('-_id title ageCategory createdAt').populate('questions', '-_id');
-   res.status(200).json({ message: 'Fetched quiz successfully.', quiz });
+    const quiz: IQuiz | null = await Quiz.findById(quizId).select('-_id title ageCategory createdAt').populate('questions', '-_id');
+    res.status(200).json({ message: 'Fetched quiz successfully.', quiz });
 };
 
 export const createQuiz = async (req: CreateQuizReq, res: Response, next: NextFunction) => {
-   // VALIDATION
-   const isSuccess = validateInputs(req, 'validation', next);
-   if (!isSuccess) return;
+    // VALIDATION
+    const isSuccess = validateInputs(req, 'validation', next);
+    if (!isSuccess) return;
 
-   const { title, ageCategory, questions } = req.body;
+    const { title, ageCategory, questions } = req.body;
+    const slug = slugify(title, { lower: true });
 
-   // CREATING QUIZ WITH NO QUESTIONS YET
-   const createdQuiz: IQuiz = new Quiz({
-      title,
-      ageCategory,
-      questions: [],
-      creatorId: req.userId
-   });
+    const existingQuizSlug: IQuiz | null = await Quiz.findOne({ slug });
+    if (existingQuizSlug) {
+	   const error = new MyError('Quiz with this title already exists.', 422);
+	   return next(error);
+    }
 
-   try {
-      for (const question of questions) {
-         // CREATING QUESTION
-         const createdQuestion: IQuestion = new Question({
-            content: question.content,
-            answers: question.answers,
-            points: question.points,
-            isMultipleChoice: question.isMultipleChoice
-         });
+    // CREATING QUIZ WITH NO QUESTIONS YET
+    const createdQuiz: IQuiz = new Quiz({
+	   title,
+	   slug,
+	   ageCategory,
+	   questions: [],
+	   creatorId: req.userId
+    });
 
-         // SAVING QUESTION AND ADDING ITS ID TO QUIZ
-         await createdQuestion.save();
-         createdQuiz.questions.push(createdQuestion._id);
-      }
+    try {
+	   for (const question of questions) {
+		  // CREATING QUESTION
+		  const createdQuestion: IQuestion = new Question({
+			 content: question.content,
+			 answers: question.answers,
+			 points: question.points,
+			 isMultipleChoice: question.isMultipleChoice
+		  });
 
-      // SAVE QUIZ TO DATABASE
-      await createdQuiz.save();
-      res.status(201).json({ message: 'Quiz successfully created.', quizId: createdQuiz._id });
-   } catch (err) {
-      catchHandler(err, next);
-   }
+		  // SAVING QUESTION AND ADDING ITS ID TO QUIZ
+		  await createdQuestion.save();
+		  createdQuiz.questions.push(createdQuestion._id);
+	   }
+
+	   // SAVE QUIZ TO DATABASE
+	   await createdQuiz.save();
+	   res.status(201).json({ message: 'Quiz successfully created.', quizId: createdQuiz._id });
+    } catch (err) {
+	   catchHandler(err, next);
+    }
 };
 
 export const editQuiz = async (req: EditQuizReq, res: Response, next: NextFunction) => {
-   // VALIDATION
-   const isSuccess = validateInputs(req, 'validation', next);
-   if (!isSuccess) return;
+    // VALIDATION
+    const isSuccess = validateInputs(req, 'validation', next);
+    if (!isSuccess) return;
 
-   // CHECKING FOR QUIZ EXISTENCE
-   const { quizId } = req.params;
-   const quiz: IQuiz | void = await validateQuizId(quizId, next);
-   if (!quiz) return;
+    // CHECKING FOR QUIZ EXISTENCE
+    const { quizId } = req.params;
+    const quiz: IQuiz | void = await validateQuizId(quizId, next);
+    if (!quiz) return;
 
-   // CHECKING AUTHORIZATION
-   if (quiz.creatorId.toString() !== req.userId!.toString()) {
-      const error = new MyError('Not authorized.', 403);
-      return next(error);
-   }
+    // CHECKING AUTHORIZATION
+    if (quiz.creatorId.toString() !== req.userId!.toString()) {
+	   const error = new MyError('Not authorized.', 403);
+	   return next(error);
+    }
 
-   const {
-      title, questions, ageCategory
-   } = req.body;
-   const prevQuestions = [...quiz.questions];
+    const {
+	   title, questions, ageCategory
+    } = req.body;
+    const prevQuestions = [...quiz.questions];
 
-   // UPDATING QUIZ
-   if (quiz.title !== title) quiz.title = title;
-   if (quiz.ageCategory !== ageCategory) quiz.ageCategory = ageCategory;
+    // UPDATING QUIZ
+    if (quiz.title !== title) quiz.title = title;
+    if (quiz.ageCategory !== ageCategory) quiz.ageCategory = ageCategory;
 
-   try {
-      for (const [idx, question] of questions.entries()) {
-         // CREATING QUESTION
-         const createdQuestion: IQuestion = new Question({
-            content: question.content,
-            answers: question.answers,
-            points: question.points,
-            isMultipleChoice: question.isMultipleChoice
-         });
+    try {
+	   for (const [idx, question] of questions.entries()) {
+		  // CREATING QUESTION
+		  const createdQuestion: IQuestion = new Question({
+			 content: question.content,
+			 answers: question.answers,
+			 points: question.points,
+			 isMultipleChoice: question.isMultipleChoice
+		  });
 
-         // SAVING QUESTION
-         await createdQuestion.save();
-         quiz.questions[idx] = createdQuestion._id;
-      }
+		  // SAVING QUESTION
+		  await createdQuestion.save();
+		  quiz.questions[idx] = createdQuestion._id;
+	   }
 
-      // SAVING QUIZ AND DELETING OLD QUESTIONS
-      await quiz.save();
-      await Question.deleteMany({ _id: { $in: prevQuestions } });
-      res.status(200).json({ message: 'Edited quiz successfully.', quizId: quiz._id });
-   } catch (err) {
-      catchHandler(err, next);
-   }
+	   // SAVING QUIZ AND DELETING OLD QUESTIONS
+	   await quiz.save();
+	   await Question.deleteMany({ _id: { $in: prevQuestions } });
+	   res.status(200).json({ message: 'Edited quiz successfully.', quizId: quiz._id });
+    } catch (err) {
+	   catchHandler(err, next);
+    }
 };
 
 export const deleteQuiz = async (req: DeleteQuizReq, res: Response, next: NextFunction) => {
-   // CHECKING FOR QUIZ EXISTENCE
-   const { quizId } = req.params;
-   const existingQuiz: IQuiz | void = await validateQuizId(quizId, next);
-   if (!existingQuiz) return;
+    // CHECKING FOR QUIZ EXISTENCE
+    const { quizId } = req.params;
+    const existingQuiz: IQuiz | void = await validateQuizId(quizId, next);
+    if (!existingQuiz) return;
 
-   // CHECKING AUTHORIZATION
-   if (existingQuiz.creatorId.toString() !== req.userId!.toString()) {
-      const error = new MyError('Not authorized.', 403);
-      return next(error);
-   }
+    // CHECKING AUTHORIZATION
+    if (existingQuiz.creatorId.toString() !== req.userId!.toString()) {
+	   const error = new MyError('Not authorized.', 403);
+	   return next(error);
+    }
 
-   try {
-      // DELETING QUIZ AND RELATED QUESTIONS
-      await Promise.all([
-         Quiz.findByIdAndDelete(quizId),
-         Rating.deleteMany({ quizId: existingQuiz._id }),
-         Question.deleteMany({ _id: { $in: existingQuiz.questions } }),
-         User.updateMany({}, { $pull: { favorites: { quizId: existingQuiz._id } } }),
-         User.updateMany({}, { $pull: { completed: { quizId: existingQuiz._id } } })
-      ]);
+    try {
+	   // DELETING QUIZ AND RELATED QUESTIONS
+	   await Promise.all([
+		  Quiz.findByIdAndDelete(quizId),
+		  Rating.deleteMany({ quizId: existingQuiz._id }),
+		  Question.deleteMany({ _id: { $in: existingQuiz.questions } }),
+		  User.updateMany({}, { $pull: { favorites: { quizId: existingQuiz._id } } }),
+		  User.updateMany({}, { $pull: { completed: { quizId: existingQuiz._id } } })
+	   ]);
 
-      res.status(200).json({ message: 'Quiz deleted successfully.', quizId });
-   } catch (err) {
-      catchHandler(err, next);
-   }
+	   res.status(200).json({ message: 'Quiz deleted successfully.', quizId });
+    } catch (err) {
+	   catchHandler(err, next);
+    }
 };
 
 export const quizToggleFavorites = async (req: DeleteQuizReq, res: Response, next: NextFunction) => {
-   // CHECKING FOR USER EXISTENCE
-   const user: IUser | void = await validateUserId(req.userId, next);
-   if (!user) return;
+    // CHECKING FOR USER EXISTENCE
+    const user: IUser | void = await validateUserId(req.userId, next);
+    if (!user) return;
 
-   // CHECKING FOR QUIZ EXISTENCE
-   const { quizId } = req.params;
-   const existingQuiz: IQuiz | void = await validateQuizId(quizId, next);
-   if (!existingQuiz) return;
+    // CHECKING FOR QUIZ EXISTENCE
+    const { quizId } = req.params;
+    const existingQuiz: IQuiz | void = await validateQuizId(quizId, next);
+    if (!existingQuiz) return;
 
-   try {
-      // GETTING INDEX (IF -1 THEN REMOVE ELSE ADD)
-      const favoriteQuizIdx = user.favorites.findIndex(quiz => quiz.quizId.toString() === quizId);
+    try {
+	   // GETTING INDEX (IF -1 THEN REMOVE ELSE ADD)
+	   const favoriteQuizIdx = user.favorites.findIndex(quiz => quiz.quizId.toString() === quizId);
 
-      if (favoriteQuizIdx !== -1) {
-         // REMOVING FROM FAVORITES
-         user.favorites.splice(favoriteQuizIdx, 1);
+	   if (favoriteQuizIdx !== -1) {
+		  // REMOVING FROM FAVORITES
+		  user.favorites.splice(favoriteQuizIdx, 1);
 
-         await user.save();
-         res.status(200).json({ message: 'Quiz removed from favorites successfully.', quizId });
-      } else {
-         // ADDING TO FAVORITES
-         const completedQuiz = user.completed.find(quiz => quiz.quizId.toString() === quizId);
-         user.favorites.push({ quizId: existingQuiz._id, isCompleted: !!completedQuiz });
+		  await user.save();
+		  res.status(200).json({ message: 'Quiz removed from favorites successfully.', quizId });
+	   } else {
+		  // ADDING TO FAVORITES
+		  const completedQuiz = user.completed.find(quiz => quiz.quizId.toString() === quizId);
+		  user.favorites.push({ quizId: existingQuiz._id, isCompleted: !!completedQuiz });
 
-         await user.save();
-         res.status(200).json({ message: 'Quiz added to favorites successfully.', quizId });
-      }
-   } catch (err) {
-      catchHandler(err, next);
-   }
+		  await user.save();
+		  res.status(200).json({ message: 'Quiz added to favorites successfully.', quizId });
+	   }
+    } catch (err) {
+	   catchHandler(err, next);
+    }
 };
 
 export const quizToggleCompleted = async (req: DeleteQuizReq, res: Response, next: NextFunction) => {
-   // VALIDATION
-   const isSuccess = validateInputs(req, 'data', next);
-   if (!isSuccess) return;
+    // VALIDATION
+    const isSuccess = validateInputs(req, 'data', next);
+    if (!isSuccess) return;
 
-   // CHECKING FOR USER EXISTENCE
-   const user: IUser | void = await validateUserId(req.userId, next);
-   if (!user) return;
+    // CHECKING FOR USER EXISTENCE
+    const user: IUser | void = await validateUserId(req.userId, next);
+    if (!user) return;
 
-   // CHECKING FOR QUIZ EXISTENCE
-   const { quizId } = req.params;
-   const existingQuiz: IQuiz | void = await validateQuizId(quizId, next);
-   if (!existingQuiz) return;
+    // CHECKING FOR QUIZ EXISTENCE
+    const { quizId } = req.params;
+    const existingQuiz: IQuiz | void = await validateQuizId(quizId, next);
+    if (!existingQuiz) return;
 
-   try {
-      // GETTING INDEX (IF -1 THEN REMOVE, SET FALSE ELSE ADD, SET TRUE)
-      const completedQuizIdx = user.completed.findIndex(quiz => quiz.quizId.toString() === quizId);
-      const favoriteQuizIdx = user.favorites.findIndex(quiz => quiz.quizId.toString() === quizId);
+    try {
+	   // GETTING INDEX (IF -1 THEN REMOVE, SET FALSE ELSE ADD, SET TRUE)
+	   const completedQuizIdx = user.completed.findIndex(quiz => quiz.quizId.toString() === quizId);
+	   const favoriteQuizIdx = user.favorites.findIndex(quiz => quiz.quizId.toString() === quizId);
 
-      if (completedQuizIdx !== -1) {
-         // SETTING COMPLETED IN FAVORITES TO FALSE, REMOVING FROM COMPLETED
-         if (favoriteQuizIdx !== -1) user.favorites[favoriteQuizIdx].isCompleted = false;
-         user.completed.splice(completedQuizIdx, 1);
+	   if (completedQuizIdx !== -1) {
+		  // SETTING COMPLETED IN FAVORITES TO FALSE, REMOVING FROM COMPLETED
+		  if (favoriteQuizIdx !== -1) user.favorites[favoriteQuizIdx].isCompleted = false;
+		  user.completed.splice(completedQuizIdx, 1);
 
-         await user.save();
-         res.status(200).json({ message: 'Quiz removed from completed successfully.', quizId });
-      } else {
-         // SETTING COMPLETED IN FAVORITES TO TRUE, ADDING TO COMPLETED
-         if (favoriteQuizIdx !== -1) user.favorites[favoriteQuizIdx].isCompleted = true;
-         const { points } = req.body;
-         user.completed.push({ quizId: existingQuiz._id, points: points! });
+		  await user.save();
+		  res.status(200).json({ message: 'Quiz removed from completed successfully.', quizId });
+	   } else {
+		  // SETTING COMPLETED IN FAVORITES TO TRUE, ADDING TO COMPLETED
+		  if (favoriteQuizIdx !== -1) user.favorites[favoriteQuizIdx].isCompleted = true;
+		  const { points } = req.body;
+		  user.completed.push({ quizId: existingQuiz._id, points: points! });
 
-         await user.save();
-         res.status(200).json({ message: 'Quiz added to completed successfully.', quizId });
-      }
-   } catch (err) {
-      catchHandler(err, next);
-   }
+		  await user.save();
+		  res.status(200).json({ message: 'Quiz added to completed successfully.', quizId });
+	   }
+    } catch (err) {
+	   catchHandler(err, next);
+    }
 };
--- backend/src/models/quiz.ts
+++ backend/src/models/quiz.ts
@@ -1,36 +1,42 @@
 import { Schema, model, Document, SchemaTypes, ObjectId } from 'mongoose';
 
 export interface IQuiz extends Document {
-   _id: ObjectId;
-   title: string;
-   questions: ObjectId[];
-   ageCategory: number;
-   creatorId: ObjectId;
-   createdAt: string;
-   editedAt: string;
+    _id: ObjectId;
+    title: string;
+    slug: string;
+    questions: ObjectId[];
+    ageCategory: number;
+    creatorId: ObjectId;
+    createdAt: string;
+    editedAt: string;
 }
 
 const quizSchema = new Schema<IQuiz>({
-   title: {
-      type: String,
-      required: true
-   },
-   questions: [
-      {
-         type: SchemaTypes.ObjectId,
-         ref: 'Question',
-         required: true,
-      }
-   ],
-   ageCategory: {
-      type: Number,
-      required: true,
-   },
-   creatorId: {
-      type: SchemaTypes.ObjectId,
-      ref: 'User',
-      required: true,
-   }
+    title: {
+	   type: String,
+	   required: true
+    },
+    slug: {
+	   type: String,
+	   unique: true,
+	   required: true
+    },
+    questions: [
+	   {
+		  type: SchemaTypes.ObjectId,
+		  ref: 'Question',
+		  required: true
+	   }
+    ],
+    ageCategory: {
+	   type: Number,
+	   required: true
+    },
+    creatorId: {
+	   type: SchemaTypes.ObjectId,
+	   ref: 'User',
+	   required: true
+    }
 }, { timestamps: true });
 
 export default model<IQuiz>('Quiz', quizSchema);
